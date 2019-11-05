29ce6a1542f012fa88083a01ed4a8b20b36ccfc0
Ethan Kaminski <ethan.kaminski@gmail.com>
2019-11-05 18:53:55 +0000

    Add 2 basic tests for one of the models in `itsystems`

--- itsystems/tests.py
+++ itsystems/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import System
 
-# Create your tests here.
+class SystemModelTest(TestCase):
+    def testAbsoluteUrl(self):
+        obj = System.objects.create(
+            id=42,
+        )
+
+        self.assertEqual('/itsystems/42/', obj.get_absolute_url())
+
+    # not clear if the behaviour of str(system_object) is specced or not, but let's test it nonetheless
+    def testStrValue(self):
+        name = "Some System"
+        obj = System.objects.create(
+            name=name,
+        )
+
+        self.assertEqual(name, str(obj))
